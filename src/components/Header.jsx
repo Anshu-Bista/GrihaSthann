67be2f38e152a16dd9705b2206a0a615b093c59a
Dawn <bistanshu3@gmail.com>
2026-01-01 13:06:49 +0545

    added navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import logo from "../assets/Logo.png";
+
+import './Header.css';
+
+export function Header({role}){
+    return(
+        <div className="header">
+                <div className="header-logo">
+                    <img src={logo} alt="Logo" />
+                </div>
+
+                <div className="mid-header">
+                    <a href="/home">Home</a>
+                    <a href="/browse">Browse</a>
+                    {role === "admin" ? (
+                        <a href="/add">Add (Admin)</a>
+                    ) : (
+                        <a href="/favourites">Favourites</a>
+                    )}
+                </div>
+
+                <div className="left-header">
+                    <a href="/profile">Profile</a>
+                </div>
+            </div>
+    );
+}
