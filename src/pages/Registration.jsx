a1901402b1f6ec67f995fe0385db34c2f46852ad
Dawn <bistanshu3@gmail.com>
2026-01-02 15:53:52 +0545

    added icons in public

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -20,123 +20,114 @@ export function Registration(){
         }
     return(
         <>
-            <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
-                {/* Name */}
-                <div className="input-group">
-                    <span className="icon user-icon"></span>
-                    <input
-                    type="text"
-                    placeholder="Full Name"
-                    {...register("name", { required: "Name is required" })}
-                    />
-                </div>
-                {errors.name && <p className="error">{errors.name.message}</p>}
-
-                {/* Email */}
-                <div className="input-group">
-                    <span className="icon email-icon"></span>
-                    <input
-                    type="email"
-                    placeholder="Email address"
-                    {...register("email", { required: "Email is required" })}
-                    />
-                </div>
-                {errors.email && <p>{errors.email.message}</p>}
-
-                {/* Phone Number */}
-                <div  className="input-group">
-                    <span className="icon phone-icon"></span>
-                    <input
-                        type="tel"
-                        placeholder="98XXXXXXXX"
-                        {...register("phone")}
-                    />
-                    {errors.phone && <p>{errors.phone.message}</p>}
-                </div>
-
-                {/* Address */}
-                <div className="input-group">
-                    <input
+            <div className="register-wrapper">
+                
+                <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+                    {/* Name */}
+                    <div className="input-group">
+                        <span className="icon user-icon"></span>
+                        <input
                         type="text"
-                        placeholder="Your address"
-                        {...register("address")}
-                    />
-                </div>
-                {errors.address && <p>{errors.address.message}</p>}
-                
-
-                {/* Gender */}
-                <div className="input-group">
-                    <label>Gender <span>(optional)</span></label>
-                    <div className="gender-group">
-                    <label>
-                        <input
-                        type="radio"
-                        value="male"
-                        {...register("gender")}
+                        placeholder="Full Name"
+                        {...register("name", { required: "Name is required" })}
                         />
-                        Male
-                    </label>
-
-                    <label>
-                        <input
-                        type="radio"
-                        value="female"
-                        {...register("gender")}
-                        />
-                        Female
-                    </label>
-
-                    <label>
-                        <input
-                        type="radio"
-                        value="other"
-                        {...register("gender")}
-                        />
-                        Other
-                    </label>
                     </div>
-                    {errors.gender && <p>{errors.gender.message}</p>}
-                </div>
+                    {errors.name && <p className="error">{errors.name.message}</p>}
 
-                {/* Password */}
-                <div className="input-group">
-                <span className="icon lock-icon"></span>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        {...register("password", {
-                        required: "Password is required"}
-                        )}
-                    />
-                </div>
-                {errors.password && <p>{errors.password.message}</p>}
-                
+                    {/* Email */}
+                    <div className="input-group">
+                        <span className="icon email-icon"></span>
+                        <input
+                        type="email"
+                        placeholder="Email address"
+                        {...register("email", { required: "Email is required" })}
+                        />
+                    </div>
+                    {errors.email && <p>{errors.email.message}</p>}
 
-                {/* Confirm Password */}
-                <div className="input-group">
-                    <span className="icon lock-icon"></span>
+                    {/* Phone Number */}
+                    <div  className="input-group">
+                        <span className="icon phone-icon"></span>
+                        <input
+                            type="tel"
+                            placeholder="98XXXXXXXX"
+                            {...register("phone")}
+                        />
+                        {errors.phone && <p>{errors.phone.message}</p>}
+                    </div>
+
+                    {/* Address */}
+                    <div className="input-group">
+                    <span className="icon location-icon"></span>
+                        <input
+                            type="text"
+                            placeholder="Your address"
+                            {...register("address")}
+                        />
+                    </div>
+                    {errors.address && <p>{errors.address.message}</p>}
                     
-                    <input
-                        type="password"
-                        placeholder="Confirm password"
-                        {...register("confirmPassword", {
-                        required: "Confirm your password"}
-                    )}
-                    />
-                </div>
-                {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-                
 
-                {/* Buttons */}
-                <Button type="submit" variant="secondary">Register</Button>
+                    {/* Gender */}
+                    <div className="gender-group">
+                        <span className="icon gender-icon"></span>
 
-                <p className="login-text">
-                    Already have an account?
-                    <Link to="/login"> Login</Link>
-                </p>
+                        <label className="gender-option">
+                            <input type="radio" value="male" {...register("gender")} />
+                            Male
+                        </label>
 
-            </form>
+                        <label className="gender-option">
+                            <input type="radio" value="female" {...register("gender")} />
+                            Female
+                        </label>
+
+                        <label className="gender-option">
+                            <input type="radio" value="other" {...register("gender")} />
+                            Other
+                        </label>
+                    </div>
+
+
+                    {/* Password */}
+                    <div className="input-group">
+                    <span className="icon lock-icon"></span>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            {...register("password", {
+                            required: "Password is required"}
+                            )}
+                        />
+                    </div>
+                    {errors.password && <p>{errors.password.message}</p>}
+                    
+
+                    {/* Confirm Password */}
+                    <div className="input-group">
+                        <span className="icon lock-icon"></span>
+                        
+                        <input
+                            type="password"
+                            placeholder="Confirm password"
+                            {...register("confirmPassword", {
+                            required: "Confirm your password"}
+                        )}
+                        />
+                    </div>
+                    {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+                    
+
+                    {/* Buttons */}
+                    <Button type="submit" variant="secondary">Register</Button>
+
+                    <p className="login-text">
+                        Already have an account?
+                        <Link to="/login"> Login</Link>
+                    </p>
+
+                </form>
+            </div>
         
         </>
     );
